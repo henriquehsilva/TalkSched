e768ad551a8bce28184c6761388f9143d737d34a
Henrique Silva <henrique@henriquesilva.dev>
2023-09-25 16:33:51 -0300

    feat: add options.merge model track

--- ts-api/app/models/track.rb
+++ ts-api/app/models/track.rb
@@ -3,4 +3,8 @@ class Track < ApplicationRecord
   validates_format_of :name, with: /\A([a-zA-Z\s]{1,})\z/
 
   has_many :talks, dependent: :destroy
+
+  def as_json(options = {})
+    super(options.merge(include: :talks))
+  end
 end
