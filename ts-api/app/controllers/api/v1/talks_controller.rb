6d44d3353bf0f1deb9428bb7789492eefda9a831
Henrique Silva <henrique@henriquesilva.dev>
2023-09-25 16:35:01 -0300

    feat: add file_format_service

--- ts-api/app/controllers/api/v1/talks_controller.rb
+++ ts-api/app/controllers/api/v1/talks_controller.rb
@@ -17,10 +17,10 @@ class Api::V1::TalksController < ApplicationController
   end
 
   def create_from_file
-    talks = extract_talks_service
-    talks.each do |row|
+    file_format_service.each do |row|
       TalksCreatorService.new(description_params(row), duration_params(row)).call
     end
+    register_final_event
     render json: Talk.all, status: :created
   end
 
@@ -45,8 +45,12 @@ class Api::V1::TalksController < ApplicationController
     params.permit(:description, :time, :duration, :track_id)
   end
 
+  def file_format_service
+    FileFormatService.new({fileFormat: params}).format
+  end
+
   def extract_talks_service_params
-    ExtractTalksService.new(params[:file])
+    ExtractTalksService.new(params)
   end
 
   def extract_talks_service
@@ -59,5 +63,10 @@ class Api::V1::TalksController < ApplicationController
 
   def duration_params(row)
     extract_talks_service_params.duration(row)
+  end
+
+  def register_final_event
+    last_time_talk = Talk.last.time + Talk.last.duration.minutes
+    Talk.create(description: 'Evento de Networking', time: last_time_talk, duration: 60, track_id: Track.last.id)
   end
 end
