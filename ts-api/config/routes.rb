b67e2950ee7574e3acee0d183972568f827d8a0b
Henrique Silva <henrique@henriquesilva.dev>
2023-09-25 10:23:14 -0300

    feat: created routes

--- ts-api/config/routes.rb
+++ ts-api/config/routes.rb
@@ -1,6 +1,14 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "api/v1/talks#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :tracks
+      resources :talks do
+        collection do
+          post :create_from_file
+        end
+      end
+    end
+  end
 end
